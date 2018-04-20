db402afe47047194fe29d9b9f67cf875a6d1aac0
BushraAlabsi <boushra-93@live.com>
2018-04-20 23:08:25 +0300

    main page added

--- client/index.js
+++ client/index.js
@@ -1,34 +1,68 @@
-angular.module('app', [])
-.controller('myCtrl', ['$scope','$http',function($scope,$http){
-$scope.component = 1
-$scope.hello="hello"
-// $scope.addClick=function (){
-// console.log($scope.number)
-// }
+var app =angular.module('app', ["ngRoute"])
+// app.config(function($routeProvider) {
+//     $routeProvider
+//     .when("/", {
+//         templateUrl : "main.html"
+//     })
+//     .when("/signup", {
+//         templateUrl : "signup.html"
+//     })
+//     .when("/login", {
+//         templateUrl : "login.html"
+//     })
+// })
+app.controller('myCtrl', ['$scope','$http','$window',function($scope,$http,$window){
+	
+// $scope.component = 1
 
-// $scope.items=["bushra","mohammed"]
+  	 this.addClickLogin = function(){
+  	var req = {
+ method: 'POST',
+ url: '/login',
+ headers: {
+   'Content-Type': 'application/json'
+ },
+ data: { username: this.loginUsername, password: this.loginPassword }
+}
+
+$http(req).then(function(data){
+	
+	
+	$window.location.href = '/main.html';
+
+}, function(err){console.log(err);
+	//this.loginPassword = null 
+alert("username or password is wrong")
+
+})}
+	this.addClick = function(){
+  	if(this.password === this.confirmPassword){
+  	var req = {
+ method: 'POST',
+ url: '/signup',
+ headers: {
+   'Content-Type': 'application/json'
+ },
+ data: { username: this.username, password: this.password , email:this.email }
+}
+
+$http(req).then(function(data){console.log(data);
+	$window.location.href = '/main.html';
+console.log("hi2")}, function(){
+	alert("username exists or empty inputs")
+	console.log("hi")});
+  }
+else {
+	alert("password doesn't match, rewrite it again");
+}}
+
+
 }])
-.component('login', {
+app.component('main', {
   controller: 'myCtrl',
-  template: `<div>
-	<table>
-		<tr>
-			<th>
-				<input type="text" ng-model="username">
-			</th>
-			<th>
-				<input type="text" ng-model="password">
-			</th>
-			<th>
-			<button type='button' class="btn btn-default center-block"> Login</button>
-			</th>				
-		</tr>
-	</table>
-</div>`
+  templateUrl: "./main.html"
 })
-.component('signup', {
+app.component('signup', {
   controller: 'myCtrl',
-  template: `<div>
-	signup
-</div>`
+  templateUrl: "signup.html"
 })
