875d7ae0063685a032c40b2e7613dfb0a4ec963f
Ahmad Mostafa <zoaby.am@gmail.com>
2018-04-23 13:50:14 +0300

    fix session bug

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-//importing the modules
-
 const express = require('express')
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -10,57 +8,44 @@ const util = require('./helpers/utility');
 const bcrypt = require('bcrypt');
 const app = express()
 
-// using the middleWares *******************************************************
-    // bodyParser middleWare
+// using of modules-------------------
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-    // session middleWare
 app.use(express.static(path.join(__dirname, 'views')))
 app.use(session({
   secret: "shhh, it is a secret",
-  resave: false,
+  resave: true,
   saveUninitialized: true
 }))
 
-// the routers *****************************************************************
+// the routes handlers----------------
 
 
-
-  // the main page route with checkUser midlleWare
-app.get('/', util.checkUser, (req, res) => {
+app.get('/', util.checkUser, function(req, res) {
    res.render('index');
 
 });
 
 
-
-
-  // take the post request for the login *******************
 app.post('/login',function(req, res){
   var userName = req.body.username;
   var password = req.body.password;
-// check the User in the database
   db.User.findOne({userName:userName}, function(err, data){
     if(err){
         console.log(err);
       }
       else {
-// if it is not exist
         if (!data) {
           res.sendStatus(404)
         }
-// if it is exist
         else {
-      // compare the given passWord with the hashed passWord
           bcrypt.compare(password, data.passWord, function(err, match){
-            // if the passWord is valid
             if(match) {
               res.status(201)
               util.createSession(req, res, data.userName);
-              console.log(req.session);
-              
+
             }
-            // if the passWord is not valid
             else {
               console.log('err');
               res.status(404)
@@ -75,23 +60,17 @@ app.post('/login',function(req, res){
 
 
 
-
-  // take the post request for the logout *******************
 app.get('/logout', function(req, res) {
   req.session.destroy(function() {
     res.sendStatus(200);
   });
-  console.log(req.session);
-})
+});
 
 
-
-   // take the post request for the signup ************************************
 app.post('/signup', function(req, res){
   let username = req.body.username;
   let password = req.body.password;
   let email = req.body.email;
-  // search for the given user in the database
   db.User.find({
     userName: username
   }, function(err, data){
@@ -100,15 +79,12 @@ app.post('/signup', function(req, res){
       console.log(err);
     }
     else {
-    // if the user   is exist in the database
       if (data.length > 0) {
         res.status(404)
         console.log('already exist');
         res.redirect('/')
       }
-    // if the user is not exist in the database
       else {
-        // hash the passWord before saving in the database
         bcrypt.genSalt(10, function (err, salt) {
         if (err) console.log(err);
         bcrypt.hash(password, salt, function(err, hash) {
@@ -117,28 +93,22 @@ app.post('/signup', function(req, res){
             passWord: hash,
             email: email
           })
-          // saving in the database
           user.save((err, data) =>{
             if (err){
               console.log(err);
             }
             else {
               console.log(data);
-              res.redirect('/')
+              util.createSession(req, res, data.userName);
+              
             }
           })
         })
       })
       }
     }
-  })
+})
 });
-
-
-
-
-
-// fetch the urls for specific user and specific category **********************
 app.post('/fetch', function (req, res){
   db.Url.find({
     category: req.body.category,
@@ -154,10 +124,6 @@ app.post('/fetch', function (req, res){
   })
 });
 
-
-
-
-// adding a url to the database ************************************************
 app.post('/add', function(req, res){
   let url = new db.Url({
   url: req.body.url,
@@ -166,6 +132,7 @@ app.post('/add', function(req, res){
   userName: req.session.user,
   likes: 0
   });
+  console.log(req.session);
   url.save(function(err,data){
     if(err){
       console.log(err)
@@ -178,10 +145,6 @@ app.post('/add', function(req, res){
 
 });
 
-
-
-
-// to dalete a specific url from the database **********************************
 app.post('/delete', function(req,res) {
   const name = req.body.name;
   db.Url.remove({urlName: name}, function(err,data){
@@ -193,9 +156,6 @@ app.post('/delete', function(req,res) {
   });
 })
 
-
-
-// get the urls for a specific user ********************************************
 app.post('/searchUser', function(req, res) {
   const username = req.body.username;
   db.Url.find({userName:username}, function(err, data) {
@@ -207,46 +167,29 @@ app.post('/searchUser', function(req, res) {
   })
 })
 
-
-
-
-// add a specific url to my urls ***********************************************
-app.post('/import', function(req, res) {
-  db.Url.findOne({userName: req.body.username, urlName:req.body.name}, function(err, data) {
-    if(err) {
-      console.log(err);
-    } else {
-      const imported = new db.Url({
-        url: data.url,
-        urlName: data.name,
-        category: data.category,
-        userName: req.session.user,
-        likes: data.likes
-      })
-      imported.save(function(err, data){
-        if(err) {
-          console.log(err);
-        } else {
-          console.log('saved ',data);
-        }
-      })
-    }
-  });
-
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
+// app.post('/import', function(req, res) {
+//   db.Url.findOne({userName: req.body.username, urlName:req.body.name}, function(err, data) {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       const imported = new db.Url({
+//         url: data.url,
+//         urlName: data.name,
+//         category: data.category,
+//         userName: req.session.user,
+//         likes: data.likes
+//       })
+//       imported.save(function(err, data){
+//         if(err) {
+//           console.log(err);
+//         } else {
+//           console.log('saved ',data);
+//         }
+//       })
+//     }
+//   });
+//
+// })
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log('Example app listening on port 3000!'))
