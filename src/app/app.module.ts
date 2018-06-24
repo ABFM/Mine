c6f7813625229ee02d902ad842845a6383e823bc
zoaby <zoaby.am@gmail.com>
2018-06-24 17:25:31 +0300

    configure the router for the home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AddComponent } from './add/add.component';
 import { MyUrlsComponent } from './my-urls/my-urls.component';
 import { HomeComponent } from './home/home.component';
+
+const appRouts: Routes = [
+  {path: '', component: HomeComponent}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +21,8 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRouts)
   ],
   providers: [],
   bootstrap: [AppComponent]
