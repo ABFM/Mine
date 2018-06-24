0cf63aced7180a7a52fa090d7404ff2bbf684085
zoaby <zoaby.am@gmail.com>
2018-06-24 23:19:36 +0300

    create the basic friends page

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friends.component.css']
 })
 export class FriendsComponent implements OnInit {
-
+friends = [
+  {
+    name: 'friend name',
+    image: 'https://www.infosecurityeurope.com/images/155_162_delegate_large.png?h=162&type=3&w=155&q=100&v=636595767522260796',
+    job: 'friend job'
+  },
+  {
+    name: 'friend name',
+    image: 'https://www.infosecurityeurope.com/images/155_162_delegate_large.png?h=162&type=3&w=155&q=100&v=636595767522260796',
+    job: 'friend job'
+  },
+  {
+    name: 'friend name',
+    image: 'https://www.infosecurityeurope.com/images/155_162_delegate_large.png?h=162&type=3&w=155&q=100&v=636595767522260796',
+    job: 'friend job'
+  },
+  {
+    name: 'friend name',
+    image: 'https://www.infosecurityeurope.com/images/155_162_delegate_large.png?h=162&type=3&w=155&q=100&v=636595767522260796',
+    job: 'friend job'
+  },
+  {
+    name: 'friend name',
+    image: 'https://www.infosecurityeurope.com/images/155_162_delegate_large.png?h=162&type=3&w=155&q=100&v=636595767522260796',
+    job: 'friend job'
+  }
+];
   constructor() { }
 
   ngOnInit() {
