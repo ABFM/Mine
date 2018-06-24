49b0a18fc48ed22e116a079446f68c6882dac4ac
zoaby <zoaby.am@gmail.com>
2018-06-24 14:06:34 +0300

    add the basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddComponent } from './add/add.component';
+import { MyUrlsComponent } from './my-urls/my-urls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddComponent,
+    MyUrlsComponent
   ],
   imports: [
     BrowserModule
