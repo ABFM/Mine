9a5141f875e83cb22bb4d190041efdb385e679b4
zoaby <zoaby.am@gmail.com>
2018-06-26 18:19:54 +0300

    complete the routing for friends

--- src/app/home/friends/friend/friend.component.ts
+++ src/app/home/friends/friend/friend.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DataService } from '../../../services/data.service';
 
 @Component({
   selector: 'app-friend',
@@ -8,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 export class FriendComponent implements OnInit {
 
   model = {};
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private data: DataService
+  ) { }
 
   ngOnInit() {
+  this.getFriendInfo();
   }
 
+  getIdFromCurrentRoute() {
+    return this.route.snapshot.params['id'];
+  }
+
+  getFriendInfo() {
+    const id = this.getIdFromCurrentRoute();
+    this.model = this.data.getFriendById(id);
+  }
 }
