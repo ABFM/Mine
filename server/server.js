37d61eed9e60ce3601c8fab6dc8359b068db7284
zoaby <zoaby.am@gmail.com>
2018-07-02 14:48:23 +0300

    organize the server folder

--- server/server.js
+++ server/server.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
 });
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Example app listening on port ', port));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log('Example app listening on PORT ', PORT));
