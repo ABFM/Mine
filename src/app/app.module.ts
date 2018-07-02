f608b5f84c0a2ab82681a0a7691e22269d933670
zoaby <zoaby.am@gmail.com>
2018-07-02 21:41:51 +0300

    arrange the app folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { FriendComponent } from './home/friends/friend/friend.component';
-import { SignupComponent } from './signup/signup.component';
-import { HeaderComponent } from './header/header.component';
-import { AddComponent } from './home/add/add.component';
-import { MyUrlsComponent } from './home/my-urls/my-urls.component';
-import { HomeComponent } from './home/home.component';
-import { UrlsNavComponent } from './home/my-urls/urls-nav/urls-nav.component';
-import { UrlComponent } from './home/my-urls/url/url.component';
-import { ProfileComponent } from './home/profile/profile.component';
-import { FriendsComponent } from './home/friends/friends.component';
-import { CategoryComponent } from './home/my-urls/category/category.component';
-import { FooterComponent } from './footer/footer.component';
+import { FriendComponent } from './components/home/friends/friend/friend.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AddComponent } from './components/home/add/add.component';
+import { MyUrlsComponent } from './components/home/my-urls/my-urls.component';
+import { HomeComponent } from './components/home/home.component';
+import { UrlsNavComponent } from './components/home/my-urls/urls-nav/urls-nav.component';
+import { UrlComponent } from './components/home/my-urls/url/url.component';
+import { ProfileComponent } from './components/home/profile/profile.component';
+import { FriendsComponent } from './components/home/friends/friends.component';
+import { CategoryComponent } from './components/home/my-urls/category/category.component';
+import { FooterComponent } from './components/footer/footer.component';
 
-import { DataService } from './services/data.service';
-import { RoutingModule } from './routing/routing.module';
-import { MaterialModule } from './material/material.module';
+import { DataService } from './services/dataService/data.service';
+import { RoutingModule } from './modules/routing/routing.module';
+import { MaterialModule } from './modules/material/material.module';
 
 
 
--- src/app/modules/routing/routing.module.ts
+++ src/app/modules/routing/routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomeComponent } from '../home/home.component';
-import { SignupComponent } from '../signup/signup.component';
-import { ProfileComponent } from '../home/profile/profile.component';
-import { AddComponent } from '../home/add/add.component';
-import { FriendsComponent } from '../home/friends/friends.component';
-import { FriendComponent } from '../home/friends/friend/friend.component';
-import { MyUrlsComponent } from '../home/my-urls/my-urls.component';
+import { HomeComponent } from '../../components/home/home.component';
+import { SignupComponent } from '../../components/signup/signup.component';
+import { ProfileComponent } from '../../components/home/profile/profile.component';
+import { AddComponent } from '../../components/home/add/add.component';
+import { FriendsComponent } from '../../components/home/friends/friends.component';
+import { FriendComponent } from '../../components/home/friends/friend/friend.component';
+import { MyUrlsComponent } from '../../components/home/my-urls/my-urls.component';
 import { RouterModule, Routes } from '@angular/router';
 
 
